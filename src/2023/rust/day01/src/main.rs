068dbb0eceb798a15be4776e18e6f2d9d1b4dcfb
hollowdll <pixerchat@gmail.com>
2023-12-08 19:58:46 +0200

    day 1 part 2

--- src/2023/rust/day01/src/main.rs
+++ src/2023/rust/day01/src/main.rs
@@ -9,6 +9,7 @@ use std::path::Path;
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -39,9 +40,48 @@ fn part1() {
         }
     }
 
-    println!("Sum: {}", sum);
+    println!("Part 1 answer: {}", sum);
 }
 
+fn part2() {
+    let letter_digits = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    let mut sum = 0;
+
+    if let Ok(lines) = read_lines("./input/input.txt") {
+        for line in lines {
+            if let Ok(line) = line {
+                let mut digits = Vec::new();
+
+                for (i, c) in line.chars().enumerate() {
+                    if c.is_digit(10) {
+                        digits.push(c.to_digit(10).unwrap() as usize);
+                    }
+                    for (j, val) in letter_digits.iter().enumerate() {
+                        if line[i..].starts_with(val) {
+                            digits.push(j + 1);
+                        }
+                    }
+                }
+
+                sum += format!("{}{}", digits[0], digits[digits.len() - 1]).parse::<i32>().unwrap();
+            }
+        }
+    }
+
+    println!("Part 2 answer: {}", sum);
+}
+
+// https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html#a-more-efficient-approach
 fn read_lines<P>(filename: P) -> io::Result<Lines<BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
